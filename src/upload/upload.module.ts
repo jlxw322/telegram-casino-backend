025f4c3fc07b9114d52f4b8b37e208c70600fdfb
Erlan-Bam <erlanzh.gg@gmail.com>
2025-11-09 21:49:29 +0500

    feat: updated

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,10 +1,8 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
-import { TelegramModule } from '../telegram/telegram.module';
 
 @Module({
-  imports: [forwardRef(() => TelegramModule)],
   controllers: [UploadController],
   providers: [UploadService],
   exports: [UploadService],
